82208119a6f0778401049b2cfa4defc4e0dffad5
Alexander Gusev <goose@alexanders-mbp.3521.dhcp.asu.edu>
2022-05-02 16:59:12 -0700

    bridge fix

--- packages/hardhat/contracts/YourContractRoot.sol
+++ packages/hardhat/contracts/YourContractRoot.sol
@@ -4,10 +4,8 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@maticnetwork/fx-portal/contracts/tunnel/FxBaseRootTunnel.sol";
 
 interface MPL {
-	function ownerOf
-		(uint tokenId)
-		external
-		returns(address);
+	function ownerOf (uint tokenId) external returns(address);
+    function getShuffledId(uint256 id) external view returns (uint256);
 }
 
 contract BootcampBridge is FxBaseRootTunnel {
@@ -15,21 +13,17 @@ contract BootcampBridge is FxBaseRootTunnel {
 	mapping(uint => bool) public isBridged;
 	MPL mpl;
 
-  constructor
-		(address _checkpointManager, address _fxRoot, MPL _mpl)
-		FxBaseRootTunnel(_checkpointManager, _fxRoot){
+  constructor(address _checkpointManager, address _fxRoot, MPL _mpl)FxBaseRootTunnel(_checkpointManager, _fxRoot){
 			mpl = _mpl;
 	}
 
-	function claimPlayers
-		(uint[] memory tokenIds)
-		public
-	{
-		for(uint i=0; i<tokenIds.length;i++){
-			require(mpl.ownerOf(tokenIds[i]) == msg.sender, 'Not owner of this token!');
-			require(isBridged[tokenIds[i]] == false, 'Already bridged this token!');
-			isBridged[tokenIds[i]] = true;
-		}
+	function claimPlayers (uint[] memory tokenIds) public {
+		for(uint i=0; i<tokenIds.length;i++) {
+            require(mpl.ownerOf(tokenIds[i]) == msg.sender, 'Not owner of this token!');
+            require(isBridged[tokenIds[i]] == false, 'Already bridged this token!');
+            isBridged[tokenIds[i]] = true;
+            tokenIds[i] = mpl.getShuffledId(tokenIds[i]);
+        }
 		_sendMessageToChild(abi.encode(tokenIds, msg.sender));
 	}
 
